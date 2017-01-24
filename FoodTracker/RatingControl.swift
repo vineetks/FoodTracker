c4810a1427d00f861f3ac38e3cc10c94d709b161
Vineet Kumar Singh <vineetkr.999@gmail.com>
2017-01-24 15:25:59 +0530

    First commit

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,57 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by VINEETKS on 1/24/17.
+//  Copyright © 2017 VINEETKS. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+    //MARK: Private Functions
+    private func setupButtons() {
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.blue
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+
+        }
+    }
+
+}
